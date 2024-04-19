4cf1ccb3ea864f2972e3d161ddf5b9c813f58c43
axolotliterature <andrewhansen.dev@gmail.com>
2024-04-18 22:52:55 -0700

    adjusted font styles on home page, need main body styling, added home-portrait css class for main portrait for finer control over border radius and responsive sizing

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -3,12 +3,12 @@ import face from '../img/hansen-1-4x3.jpg'
 export default function Home() {
     return <>
     <div className="w-full h-full border-2 grid text-center">
-        <h1 className="nothing-font name-title text-cyan-600">Andrew Hansen</h1>
+        <h1 className="nothing-font name-title text-gray-300">Andrew Hansen</h1>
         <h2>Programmer</h2>
 
         <div id="about-container" className="grid grid-cols-2 w-10/12 text-center m-auto">
             <div id="about-left" className="border-4 border-blue-300">
-                <h1>Education</h1>
+                <h1 className="">Education</h1>
                 <p>B.A. in Digital Technology and Culture</p>
                 <p>Minor in English</p>
                 <p>Certification in Game Studies and Design</p>
@@ -30,7 +30,7 @@ export default function Home() {
             </div>
 
             <div id="about-right" className="border-4 border-red-300">
-                <img src={face} alt="placeholder" className="m-auto min-w-24 max-w-lg rounded-full shadow-md shadow-gray-500/70" />
+                <img src={face} alt="placeholder" className="home-portrait m-auto shadow-md shadow-gray-500/70" />
 
             </div>
         </div>
