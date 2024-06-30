164be14e457595073a6fd0f1979a9da03594f8c6
axolotliterature <andrewhansen.dev@gmail.com>
2024-06-30 15:56:07 -0700

    Removed photo from home, began restyling home page
    Stripped away some of the tailwind styling and simplified with standard css for repeatability. Styled home page with CSS grid to separate fields. Adjusted positioning of information for improved eye tracking based on usability and UI feedback.

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -1,24 +1,41 @@
-import face from '../img/hansen-1-4x3.jpg'
+// import face from '../img/hansen-1-4x3.jpg'
 import resume from '../documents/andrew-hansen-resume.pdf'
+
+import emailIcon from '../img/icons/mail.png'
+import githubIcon from '../img/icons/github.png'
+import instagramIcon from '../img/icons/instagram.png'
+import linkedinIcon from '../img/icons/linkedin.png'
 
 export default function Home() {
     return <>
-    <div className="w-full h-full bg-gray-900 grid text-center">
-        <h1 className="nothing-font name-title text-gray-300">Andrew Hansen</h1>
-        <h2 className="text-cyan-600">Game Developer | Software Engineer | Web Developer</h2>
-        <p className="text-gray-600 pb-3">Looking for a junior developer/engineer role so that I can *karate chops the air* continue perfecting my craft.</p>
+    <div className="w-full h-full bg-gray-900 grid">
+        <div id="about-header" className="text-center pb-8">
+            <h1 className="nothing-font name-title text-gray-300">Andrew Hansen</h1>
+            <h3 className="text-cyan-400">Web Developer | Programmer | Game Developer</h3>
+            <h3 className="py-2">
+                <a className="px-5" href="mailto:andrewhansen.dev@gmail.com"><img src={emailIcon} alt="Email Icon" className="contact-icon" /></a> 
+                <a className="px-5" href="https://github.com/axolotliterature" rel="noreferrer noopener" target="_blank"><img src={githubIcon} alt="Github Icon" className="contact-icon" /></a> 
+                <a className="px-5" href="https://www.linkedin.com/in/andrewhansen-dev/" rel="noreferrer noopener" target="_blank"><img src={linkedinIcon} alt="LinkedIn Icon" className="contact-icon" /></a>
+                <a className="px-5" href="https://www.instagram.com/td.grim/" rel="noreferrer noopener" target="_blank"><img src={instagramIcon} alt="Instagram Icon" className="contact-icon" /></a>
+            </h3>
+        </div>
 
         <div id="about-container">
-            <div id="about-top" className="grid grid-cols-2 w-10/12 text-center mx-auto"> 
+            <div id="about-main" className="grid grid-cols-2 w-10/12 mx-auto"> 
                 <div id="about-left" className="">
-                    <h1 className="title-font font-medium text-white mb-1">Education</h1>
-                    <p className="text-cyan-600">B.A. in Digital Technology and Culture</p>
+
+                    {/* LEFT SIDE FOR INTRO AND CONTACT */}
+
+                    {/* <h1 className="title-font font-medium text-white mb-1">Education</h1> */}
+                    <h3 className="text-white">B.A. Digital Technology and Culture</h3>
                     <p>The CMDC of the Department of Digital Technology and Culture</p>
                     <p>Minor in English</p>
-                    <p>Certification in Game Studies and Design</p>
-                    <p className="text-red-800">Washington State University Vancouver</p>
+                    <p className="pb-5">Washington State University Vancouver</p>
+
+                    <h3 className="text-white">Certification in Game Studies and Design</h3>
+                    <p>Washington State University Vancouver</p>
                     
-                    <h1 className="title-font font-medium text-white mb-1 pt-4">Proficiencies</h1>
+                    {/* <h1 className="title-font font-medium text-white mb-1 pt-4">Proficiencies</h1>
                     <h3 className="text-cyan-600 pt-3">Game Development and Design</h3>
                     <p>Unreal Engine, Unity, C#, GameMaker, p5.js, JavaScript</p>
 
@@ -29,15 +46,52 @@ export default function Home() {
                     <p>Adobe: Illustrator, Photoshop, Lightroom, Premiere Pro, Blender, Davinci Resolve, Ableton Live</p>
 
                     <h3 className="text-cyan-600 pt-3">Collaboration and Version Control</h3>
-                    <p>Github, Perforce Helix Core, Slack, Basecamp, Zoom, OBS Studio</p>
+                    <p>Github, Perforce Helix Core, Slack, Basecamp, Zoom, OBS Studio</p> */}
+
                 </div>
 
                 <div id="about-right" className="">
-                    <img src={face} alt="placeholder" className="home-portrait m-auto shadow-md shadow-gray-500/70" />
+
+                    {/* RIGHT SIDE FOR EXPERIENCE BRIEF AND RESUME LINK */}
+                    {/* as a lifelong tinkerer... former career mechanic,  */}
+
+                    <p className="pb-2"> Aute non ipsum culpa laborum mollit sunt exercitation. Excepteur occaecat qui anim tempor laboris irure. Cupidatat esse ipsum dolor ad irure enim id ea quis qui elit exercitation proident. Sit ea eiusmod deserunt nostrud eu nulla cillum aute amet ipsum. Adipisicing labore minim sint Lorem ullamco ad magna reprehenderit. Velit commodo labore do non voluptate in ullamco laboris fugiat ut labore. Lorem ullamco fugiat magna minim laborum fugiat commodo duis reprehenderit dolor. </p>
+
+                    <p> Aute non ipsum culpa laborum mollit sunt exercitation. Excepteur occaecat qui anim tempor laboris irure. Cupidatat esse ipsum dolor ad irure enim id ea quis qui elit exercitation proident. Sit ea eiusmod deserunt nostrud eu nulla cillum aute amet ipsum. Adipisicing labore minim sint Lorem ullamco ad magna reprehenderit. Velit commodo labore do non voluptate in ullamco laboris fugiat ut labore. Lorem ullamco fugiat magna minim laborum fugiat commodo duis reprehenderit dolor. </p>
+
+                    <div id="about-right-resume">
+                    <h3 className="p-1 text-cyan-400 text-center"><a href={resume} rel="noreferrer noopener" target="_blank">View my CV Resume</a></h3>
+                        <div className="exp-block">
+                            <div id="" className="exp-left">
+                                <p>2024-Present</p>
+                            </div>
+
+                            <div id="" className="exp-right">
+                                <h3>Experience 1 Title - Company Name</h3>
+                                <p>Dolor officia ullamco enim qui qui amet cupidatat eu labore eu ut.</p>
+                                <p>Fugiat incididunt id incididunt sunt magna fugiat velit non do minim.</p>
+                                <p>Velit eiusmod cupidatat sint ipsum do ad nisi excepteur reprehenderit.</p>
+                            </div>
+                        </div>
+
+                        <div className="exp-block">
+                            <div id="" className="exp-left">
+                                <p>2023-2024</p>
+                            </div>
+
+                            <div id="" className="exp-right">
+                                <h3>Experience 2 Title - Company Name</h3>
+                                <p>Dolor officia ullamco enim qui qui amet cupidatat eu labore eu ut.</p>
+                                <p>Fugiat incididunt id incididunt sunt magna fugiat velit non do minim.</p>
+                                <p>Velit eiusmod cupidatat sint ipsum do ad nisi excepteur reprehenderit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
 
-            <div id="about-bottom" className="mx-auto w-10/12 text-center pt-10">
+            {/* <div id="about-bottom" className="mx-auto w-10/12 text-center pt-10">
 
                 <h2 className="text-white text-2xl font-medium underline">Contact</h2>
 
@@ -49,7 +103,7 @@ export default function Home() {
 
                 <h3 className="p-1 text-indigo-500"><a href={resume} rel="noreferrer noopener" target="_blank">View my CV Resume</a></h3>
 
-            </div>
+            </div> */}
         </div>
     </div>
     
