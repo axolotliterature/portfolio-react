044cedbc018dd36192952d0f836462681b104c31
axolotliterature <andrewhansen.dev@gmail.com>
2024-07-27 13:28:05 -0700

    Removed old commented out sections, updated some finer tuned texts on the home page introduction and experience sections

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -27,33 +27,17 @@ export default function Home() {
                 <div id="about-left" className="">
 
                     {/* LEFT SIDE FOR INTRO AND CONTACT */}
-
-                    {/* <h1 className="title-font font-medium text-white mb-1">Education</h1> */}
                     
                     <p className="pb-5"> As a lifelong tinkerer, I've had an interest in computers and electronics since I was a child. From building custom PCs, to modifying game files, I've casually stoked a life-long interest in computers. Like many others, I had an introduction to HTML through customizing social media pages, an introduction to JavaScript by early web-based games, and an introduction to command line by bricking my parents' computer more than a few times before I got it right. Fortunately, we still had the boot CD. Sorry Mom. Despite focusing on Information Technology for my Associates Degree I remained somewhat casual in my approach to technology, instead being driven by a love of cars through a career as an automotive technician. When I felt the call to pursue a new profession that allowed for further personal growth, I found myself returning to school to complete my Bachelors in Digital Technology with a focus on Web and Game Development.</p>
 
                     <p className="pb-5">After graduating from WSU in 2023 I took some time to focus on a few personal projects, taking some detours along the way to try out many different languages and frameworks to see what I liked best. Despite bouncing back and forth between C# and Python, I found myself continuously returning to JavaScript and its plethora of libraries and frameworks for the majority of my projects. I am excited to continue learning new skills and technologies, and exploring new ways of storytelling and expression through games and software. However, I am also eager to join a team that encourages personal growth and enables me to hone valuable professional skills, where I can put my curiosity and expertise to use contributing to a wider goal.
                     </p>
-                    
-                    {/* <h1 className="title-font font-medium text-white mb-1 pt-4">Proficiencies</h1>
-                    <h3 className="text-cyan-600 pt-3">Game Development and Design</h3>
-                    <p>Unreal Engine, Unity, C#, GameMaker, p5.js, JavaScript</p>
-
-                    <h3 className="text-cyan-600 pt-3">Software and Web Development</h3>
-                    <p className="">JavaScript, React.js, Node.js, C#, .NET, HTML5, CSS3</p>
-
-                    <h3 className="text-cyan-600 pt-3">Multimedia Creation</h3>
-                    <p>Adobe: Illustrator, Photoshop, Lightroom, Premiere Pro, Blender, Davinci Resolve, Ableton Live</p>
-
-                    <h3 className="text-cyan-600 pt-3">Collaboration and Version Control</h3>
-                    <p>Github, Perforce Helix Core, Slack, Basecamp, Zoom, OBS Studio</p> */}
 
                 </div>
 
                 <div id="about-right" className="">
 
                     {/* RIGHT SIDE FOR EXPERIENCE BRIEF AND RESUME LINK */}
-                    {/* as a lifelong tinkerer... former career mechanic,  */}
 
                     <h3 className="text-white">B.A. Digital Technology and Culture</h3>
                     <p>The CMDC of the Department of Digital Technology and Culture</p>
@@ -75,7 +59,7 @@ export default function Home() {
                             </div>
 
                             <div className="exp-right">
-                                <h3>CAD Programmer - D&D Home Interiors</h3>
+                                <h3 className="text-white">CAD Programmer - D&D Home Interiors</h3>
                                 <p>Pushed to streamline internal communication channels via the introduction of new technologies and strategies. Assisted in the identication and reduction of pre-production bottlenecks. Create clean and concise process documentation. Evaluate, test, and debug programs to ensure performance and accuracy. Manage API integrations to maximize application efficiency. EOTM October 2023</p>
                             </div>
                         </div>
@@ -86,7 +70,7 @@ export default function Home() {
                             </div>
 
                             <div className="exp-right">
-                                <h3>Gameplay Programmer - DATA ENTRY: PORTAL</h3>
+                                <h3 className="text-white">Gameplay Programmer - DATA ENTRY: PORTAL</h3>
                                 <p>Engaged heavily in the game design pre-production cycle, proactively prototyping new ideas and solutions which helped our team rocket from
                                 conceptualization to playable demo in only four months. Sequenced animations for puzzles, UI updates, and particle effects. Programmed scriptable object events for level transitions and puzzle interactions. Implemented player interactions and locomotion.</p>
                             </div>
@@ -98,29 +82,13 @@ export default function Home() {
                             </div>
 
                             <div className="exp-right">
-                                <h3>Automotive Technician - Subaru of America Dealerships</h3>
+                                <h3 className="text-white">Automotive Technician - Subaru of America Dealerships</h3>
                                 <p>ASE Certified Specialist for Engine, Transmission, Brake, and Electrical diagnostic and repair. Subaru of America Certified Boxer Engines Specialist, Transmissions Specialist. Collaboration with Subaru of America engineers for product quality monitoring. Read and interpret advanced schematics and wiring diagrams to efficiently reach diagnosis and accurately propose recommended repairs.</p>
                             </div>
                         </div>
-
                     </div>
-                    
                 </div>
             </div>
-
-            {/* <div id="about-bottom" className="mx-auto w-10/12 text-center pt-10">
-
-                <h2 className="text-white text-2xl font-medium underline">Contact</h2>
-
-                <h3 className="p-1 text-cyan-500"><a href="mailto:andrewhansen.dev@gmail.com">Email</a></h3>
-
-                <h3 className="p-1 text-sky-500"><a href="https://github.com/axolotliterature" rel="noreferrer noopener" target="_blank">Github</a></h3>
-
-                <h3 className="p-1">Find me on <a className="text-blue-500" href="https://www.linkedin.com/in/andrewhansen-dev/" rel="noreferrer noopener" target="_blank">Linkedin</a></h3>
-
-                <h3 className="p-1 text-indigo-500"><a href={resume} rel="noreferrer noopener" target="_blank">View my CV Resume</a></h3>
-
-            </div> */}
         </div>
     </div>
     
